98cbf0cde4765e694986a455fa0a77d0b507d3d1
atomic <baileyweb04@gmail.com>
2024-05-07 22:17:10 -0400

    win condition

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ function App() {
   const [dice, setDice] = useState(DiceRoll())
   const [tenziesWin, setTenziesWin] = useState(false)
 
+  //Handles the win condition. It checks if the conditions are met each time the dice state is changed
   useEffect(() => {
-    const winCondition = dice.every(item => item.isPicked && item.value)
+    const winCondition = dice.every(item => item.isPicked && item.value === dice[0].value)
 
     if(winCondition){
       setTenziesWin(true)
@@ -52,8 +53,8 @@ function App() {
     <div className='game-container'>
       {tenziesWin && <Confetti/>}
       <h1>Tenzies</h1>
-      <p>When all the numbers match you win</p>
-      <Dice dice={dice} setDice={setDice}/>
+      <p>Roll the dice until all the numbers match. Click on a number to freeze it</p>
+      <Dice dice={dice} setDice={setDice} tenziesWin={tenziesWin}/>
       {tenziesWin ? <button className='btn btn-warning' onClick={ResetGame}>Replay</button> : <button className='btn btn-success' onClick={GenerateNewRoll}>Roll</button>}
     </div>
   )
--- src/componet/Dice.jsx
+++ src/componet/Dice.jsx
@@ -2,14 +2,19 @@ function Dice(props){
 
   const mapDiceArray = props.dice.map((item, index) => {
 
+    //Dice styles
     const dieStyle = {
-      backgroundColor: item.isPicked ? '#58e390' : 'white'
+      backgroundColor: item.isPicked ? '#58e390' : 'white',
+      boxShadow: item.isPicked ? "inset rgba(0, 0, 0, 0.06) 0px 2px 4px 0px" : "rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px"
     }
 
+    //If win condition hasent been met yet, this allows the player to select and deselect dice to hold
     function changeHeld(index) {
-      const newarr = [...props.dice]
-      newarr[index] = {...newarr[index], isPicked: !newarr[index].isPicked}
-      props.setDice(newarr)
+      if(!props.tenziesWin){
+        const newarr = [...props.dice]
+        newarr[index] = {...newarr[index], isPicked: !newarr[index].isPicked}
+        props.setDice(newarr)
+      }
     }
   
 
